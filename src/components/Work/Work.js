80978c34b873782a01b8f865f1629e03c2aa540b
Md. Imriaz Uddin <imriaz.cse.bubt@gmail.com>
2022-01-10 20:19:00 +0600

    task3 done

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -4,24 +4,24 @@ import work from '../../images/work.png'
 
 const Work = () => {
     return (
-        <div class="work-container">
-            <div class="row">
-                <div class="col-6">
-                    <h1 className='work-text text-start p-3'>What we do</h1>
-                    <p className='work-text-details text-start p-3'>Redesigning of an STR software
-                        i.e, which will help property owner to make the
-                        short-term property rental process  completely
-                        complaint  i.e making the process completely
-                        comply with the government defined rules, regulations
-                        and policies. Not only this but this website also provide
-                        holistic approach for the client to manage their
-                        end-to-end rental services, tracking and keeping
-                        it complaint</p>
-                </div>
-                <div class="col-6">
-                    <img className='work-img' src={work} alt="" /></div>
+        <div class="work-container row">
+            {/* <div class="row"> */}
+            <div class="col-md-6 col-12">
+                <h1 className='work-text text-start p-3'>What we do</h1>
+                <p className='work-text-details text-start p-3'>Redesigning of an STR software
+                    i.e, which will help property owner to make the
+                    short-term property rental process  completely
+                    complaint  i.e making the process completely
+                    comply with the government defined rules, regulations
+                    and policies. Not only this but this website also provide
+                    holistic approach for the client to manage their
+                    end-to-end rental services, tracking and keeping
+                    it complaint</p>
             </div>
+            <div class="col-md-6 col-12">
+                <img className='work-img' src={work} alt="" /></div>
         </div>
+        // </div>
     );
 };
 
